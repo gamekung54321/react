7c9adaf293a305154970d07d075583a1657c9983
anawin <gamekung54321@gmail.com>
2021-02-17 12:27:55 +0700

    react

---
+++ test/src/Appheader.js
@@ -0,0 +1,8 @@
+function Appheader(){
+    return (
+        <header className="App-header">
+            <img className = "header-logo" src = "/pic/1.jpg"/>
+        </header>
+    );
+}
+export default Appheader;
---
+++ test/src/items.js
@@ -0,0 +1,27 @@
+const items =[
+    {
+      title :"man",
+      thumbnaiUrl :"/pic/1.jpg"
+      
+    
+    },
+    {
+      title :"gril",
+      thumbnaiUrl :"/pic/2.jpg"
+      
+    
+    },
+    {
+      title :"boy",
+      thumbnaiUrl :"/pic/3.png"
+      
+    
+    },
+    {
+      title :"view",
+      thumbnaiUrl :"/pic/4.png"
+      
+    
+    }
+  ];
+  export default items;
